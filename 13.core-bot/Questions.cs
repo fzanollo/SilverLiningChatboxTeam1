6aa5e4f8026a2b85552e26d336d7edac466028d6
Fzanollo <fzanollo@dc.uba.ar>
2020-07-29 20:25:55 -0300

    infinite questions

--- 13.core-bot/Questions.cs
+++ 13.core-bot/Questions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Bot.Schema;
 using Newtonsoft.Json;
@@ -9,49 +10,44 @@ namespace QuestionsOverview
 {
     class Questions
     {
-      private List<Tuple<string,int>> qst = new List<Tuple<string,int>>()
+      private HashSet<Tuple<string,int>> qst = new HashSet<Tuple<string,int>>()
       {
         Tuple.Create("what's 5x5",25),
         Tuple.Create("what's 6x6",36),
         Tuple.Create("what's 7x7",49),
+        Tuple.Create("what 1x1",1),
+        Tuple.Create("what 2x2",4),
+        Tuple.Create("what 3x3",9),
+        Tuple.Create("what 4x4",16),
+        Tuple.Create("what 1x2",2),
+        Tuple.Create("what 1x3",3),
+        Tuple.Create("what 4x3",12),
+        Tuple.Create("what 2x3",6),
+        Tuple.Create("what 8x7",56),
+        Tuple.Create("what 1+1",2),
+        Tuple.Create("what 1-1",0),
+        Tuple.Create("what 7+7",14),
+        Tuple.Create("what 2x7",14),
+        Tuple.Create("what 4x5",20),
+        Tuple.Create("what 12x2",24),
       };
-        private int index;
-
-        private int listSize = 3;
 
         private static Random rng;
-
-        private static void Shuffle(List<Tuple<string,int>> list)
-        {
-            int n = list.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = rng.Next(n + 1);
-                Tuple<string,int> value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
 
         public Questions()
         {
             rng = new Random();
-            Shuffle(qst);
-            index = 0;
         }
 
         public Tuple<string,int> getQuestion()
         {
-            Tuple<string, int> tp = (index < listSize) ? (Tuple.Create(qst[index].Item1, qst[index].Item2)) : (Tuple.Create("error", -1));
-            index++;
-            return tp;
+            Tuple<string, int> element = qst.ElementAt(rng.Next(qst.Count));
+            return element;
         }
 
         public void addQuestion(Tuple<string,int> tp)
         {
             qst.Add(tp);
-            listSize++;
         }
         
     }
