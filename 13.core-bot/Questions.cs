ed66a8fa190728caa95a5208f83b0166baea11bd
Miguel Elizondo <miguelelizondov@gmail.com>
2020-07-29 13:54:26 -0500

    Random improve

--- 13.core-bot/Questions.cs
+++ 13.core-bot/Questions.cs
@@ -21,14 +21,14 @@ namespace QuestionsOverview
 
         private static Random rng;
 
-        private static void Shuffle<T>(List<T> list)
+        private static void Shuffle(List<Tuple<string,int>> list)
         {
             int n = list.Count;
             while (n > 1)
             {
                 n--;
                 int k = rng.Next(n + 1);
-                T value = list[k];
+                Tuple<string,int> value = list[k];
                 list[k] = list[n];
                 list[n] = value;
             }
